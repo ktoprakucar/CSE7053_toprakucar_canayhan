1795542f7ada043e746afecde946b30b0a121c49
Kemal Toprak Uçar <toprakucar@gmail.com>
2017-12-04 23:35:11 +0300

    add: graph generator using node and edge list

--- sna-employee-manager-analysis/Application.py
+++ sna-employee-manager-analysis/Application.py
@@ -1,11 +1,20 @@
+from network.Generator import Generator
 from reader.CsvReader import CsvReader
 
+import networkx as nx
 
 class Application:
-
+    networkGenerator = Generator()
     reader = CsvReader()
+
     nodeList = reader.readEmployee()
     tesekkurList = reader.readRelationship('tesekkur')
     takdirList = reader.readRelationship('takdir')
     dogumgunuList = reader.readRelationship('dogumgunu')
-    print("blabla")
+    relationships = tesekkurList + takdirList + dogumgunuList
+
+    graph = networkGenerator.generateGraph(nodeList, relationships)
+
+    print(graph.number_of_nodes())
+    print(graph.number_of_edges())
+
---
+++ sna-employee-manager-analysis/network/Generator.py
@@ -0,0 +1,12 @@
+import networkx as nx
+
+class Generator:
+
+    def generateGraph(self, nodeList, relationshipList):
+        graph = nx.MultiDiGraph()
+        for node in nodeList:
+            graph.add_node(node.id)
+        print(len(relationshipList))
+        for r in relationshipList:
+            graph.add_edge(r.fromNode, r.toNode, type=r.type)
+        return graph

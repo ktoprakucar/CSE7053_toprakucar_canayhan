9ff6a76fb105b9962c38be92e9e6ef4dc0c68247
Kemal Toprak Uçar <toprakucar@gmail.com>
2017-12-26 00:00:00 +0300

    add: number of relations from node and the number of the relations from specific node to another specific node

--- sna-employee-manager-analysis/network/TieStrength.py
+++ sna-employee-manager-analysis/network/TieStrength.py
@@ -12,9 +12,9 @@ class TieStrength:
         graph = nx.DiGraph()
 
         # add takdir
-        self.generateGraphFromRelations(graph, self.takdirList, 1000)
+        self.generateGraphFromRelations(graph, self.takdirList, 1)
         # add tesekkur
-        self.generateGraphFromRelations(graph, self.tesekkurList, 1 / 5)
+        self.generateGraphFromRelations(graph, self.tesekkurList, 1)
         # add dogumgunu
         self.generateGraphFromRelations(graph, self.dogumgunuList, 1)
 
@@ -28,9 +28,9 @@ class TieStrength:
         graph = nx.DiGraph()
 
         # add takdir
-        self.generateGraphFromRelations(graph, self.takdirList, 1000)
+        self.generateGraphFromRelations(graph, self.takdirList, 1)
         # add tesekkur
-        self.generateGraphFromRelations(graph, self.tesekkurList, 1 / 5)
+        self.generateGraphFromRelations(graph, self.tesekkurList, 1)
         # add dogumgunu
         self.generateGraphFromRelations(graph, self.dogumgunuList, 1)
 
@@ -62,8 +62,22 @@ class TieStrength:
                 graph.remove_edge(edge[0], edge[1])
 
     def generateGraphFromRelations(self, graph, list, calibrationValue):
+        numberOfEdgesFromNode = {}
+        self.calculateEdgeNumberFromTheNode(numberOfEdgesFromNode, list)
         for relation in list:
             if graph.has_edge(relation.fromNode, relation.toNode):
-                graph[relation.fromNode][relation.toNode]['weight'] += relation.point / calibrationValue
+
+                # if there are more than one relation between same node, weights of these relations are decreased
+                graph[relation.fromNode][relation.toNode]['weight'] = (graph[relation.fromNode][relation.toNode]['weight'] * numberOfEdgesFromNode[relation.fromNode] * calibrationValue) * graph[relation.fromNode][relation.toNode]['numberOfEdges'] * 0.1
+                graph[relation.fromNode][relation.toNode]['weight'] += relation.point
+                graph[relation.fromNode][relation.toNode]['numberOfEdges'] += 1
+                graph[relation.fromNode][relation.toNode]['weight'] = (graph[relation.fromNode][relation.toNode]['weight']/numberOfEdgesFromNode[relation.fromNode]) / (graph[relation.fromNode][relation.toNode]['numberOfEdges'] * 0.1 * calibrationValue)
             else:
-                graph.add_edge(relation.fromNode, relation.toNode, weight=relation.point / calibrationValue)
+                graph.add_edge(relation.fromNode, relation.toNode, weight=(relation.point / numberOfEdgesFromNode[relation.fromNode] ) / calibrationValue, numberOfEdges=1)
+
+    def calculateEdgeNumberFromTheNode(self, numberOfEdgesFromNode, list):
+        for relation in list:
+            if relation.fromNode in numberOfEdgesFromNode:
+                numberOfEdgesFromNode[relation.fromNode] += 1
+            else:
+                numberOfEdgesFromNode[relation.fromNode] = 1

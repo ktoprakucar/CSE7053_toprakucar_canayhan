4c0c6d8b412663e3f540a50ca27fbb26bcb646d7
Kemal Toprak Uçar <toprakucar@gmail.com>
2018-01-06 20:46:50 +0300

    wip: tie strenght

--- sna-employee-manager-analysis/network/TieStrength.py
+++ sna-employee-manager-analysis/network/TieStrength.py
@@ -1,4 +1,5 @@
 import networkx as nx
+import math
 
 
 class TieStrength:
@@ -11,12 +12,13 @@ class TieStrength:
     def calculateFromManagerToEmployee(self):
         graph = nx.DiGraph()
 
+
         # add takdir
-        self.generateGraphFromRelations(graph, self.takdirList, 1)
+        self.generateGraphFromRelations(graph, self.takdirList, 1 / len(self.takdirList))
         # add tesekkur
-        self.generateGraphFromRelations(graph, self.tesekkurList, 1)
+        self.generateGraphFromRelations(graph, self.tesekkurList, 1 / len(self.tesekkurList))
         # add dogumgunu
-        self.generateGraphFromRelations(graph, self.dogumgunuList, 1)
+        self.generateGraphFromRelations(graph, self.dogumgunuList, 1 / len(self.dogumgunuList))
 
         print("before removing nonmanagers: " + str(len(graph.edges)))
         self.removeNonManagerSenders(graph)
@@ -28,11 +30,11 @@ class TieStrength:
         graph = nx.DiGraph()
 
         # add takdir
-        self.generateGraphFromRelations(graph, self.takdirList, 1)
+        self.generateGraphFromRelations(graph, self.takdirList, 1 / len(self.takdirList))
         # add tesekkur
-        self.generateGraphFromRelations(graph, self.tesekkurList, 1)
+        self.generateGraphFromRelations(graph, self.tesekkurList, 1 / len(self.tesekkurList))
         # add dogumgunu
-        self.generateGraphFromRelations(graph, self.dogumgunuList, 1)
+        self.generateGraphFromRelations(graph, self.dogumgunuList, 1 / len(self.dogumgunuList))
 
         print("before removing managers: " + str(len(graph.edges)))
         self.removeManagerSenders(graph)
@@ -64,17 +66,39 @@ class TieStrength:
     def generateGraphFromRelations(self, graph, list, calibrationValue):
         numberOfEdgesFromNode = {}
         self.calculateEdgeNumberFromTheNode(numberOfEdgesFromNode, list)
+        greatestWeight = self.getGreatestWeight(list)
         for relation in list:
+            normalizedWeight = self.normalizeWeight(relation.point, greatestWeight)
+            calculatedWeight = normalizedWeight * (10 / numberOfEdgesFromNode[relation.fromNode]) * calibrationValue
             if graph.has_edge(relation.fromNode, relation.toNode):
-
-                # if there are more than one relation between same nodes, weights of these relations are decreased
-                # log (x+1) / log(max(x)) +1)
-                graph[relation.fromNode][relation.toNode]['weight'] = (graph[relation.fromNode][relation.toNode]['weight'] * numberOfEdgesFromNode[relation.fromNode] * calibrationValue) * graph[relation.fromNode][relation.toNode]['numberOfEdges'] * 0.1
-                graph[relation.fromNode][relation.toNode]['weight'] += relation.point
+                graph[relation.fromNode][relation.toNode]['weight'] += calculatedWeight
                 graph[relation.fromNode][relation.toNode]['numberOfEdges'] += 1
-                graph[relation.fromNode][relation.toNode]['weight'] = (graph[relation.fromNode][relation.toNode]['weight']/numberOfEdgesFromNode[relation.fromNode]) / (graph[relation.fromNode][relation.toNode]['numberOfEdges'] * 0.1 * calibrationValue)
             else:
-                graph.add_edge(relation.fromNode, relation.toNode, weight=(relation.point / numberOfEdgesFromNode[relation.fromNode] ) / calibrationValue, numberOfEdges=1)
+                graph.add_edge(relation.fromNode, relation.toNode, weight= calculatedWeight, numberOfEdges=1)
+        mostRelationAmount = self.retrieveMostRelationBetweenSameNodes(graph)
+        self.relateEdgeSizeOnWeights(graph, mostRelationAmount)
+
+    def relateEdgeSizeOnWeights(self, graph, mostRelationAmount):
+        for edge in graph.edges():
+            graph[edge[0]][edge[1]]['weight'] = graph[edge[0]][edge[1]]['weight'] / (
+            math.log(graph[edge[0]][edge[1]]['numberOfEdges'] + 1, mostRelationAmount))
+
+    def retrieveMostRelationBetweenSameNodes(self, graph):
+        relationAmount = 0
+        for edge in graph.edges():
+            if graph[edge[0]][edge[1]]['numberOfEdges'] > relationAmount:
+                relationAmount = graph[edge[0]][edge[1]]['numberOfEdges']
+        return relationAmount
+
+    def getGreatestWeight(self, list):
+        greatestWeight = 0
+        for relation in list:
+            if relation.point > greatestWeight:
+                greatestWeight = relation.point
+        return greatestWeight
+
+    def normalizeWeight(self, weight, greatestWeight):
+        return math.log10(weight + 1) / (math.log10(greatestWeight) + 1)
 
     def calculateEdgeNumberFromTheNode(self, numberOfEdgesFromNode, list):
         for relation in list:
